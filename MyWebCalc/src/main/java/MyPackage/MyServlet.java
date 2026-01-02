36e7d578a14fb483eb91369e90041c71abc347a1
Anshumala Pandit <anshumalapandit@gmail.com>
2026-01-02 17:35:06 +0530

    Initial commit – Java Servlet JSP project

---
+++ MyWebCalc/src/main/java/MyPackage/MyServlet.java
@@ -0,0 +1,75 @@
+package MyPackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String[] jokes = {
+	        "Why do Java developers wear glasses? Because they don't C#!",
+	        "I told my computer I needed a break, and now it won't stop sending me Kit Kat bars.",
+	        "Why don't programmers like nature? It has too many bugs.",
+	        "How many programmers does it take to change a light bulb? None, it's a hardware problem.",
+	        "Why Programmers Like Dark Mode ? because light attracts bugs ! ",
+	        "Why did the integer drown? Because it couldn’t float.",
+	        "What do you call if a programmer gets an interview because of a recommendation?\r\n"
+	        + "Then they being passed by reference.",
+	        "As a programmer, sometimes I feel a void.\r\n"
+	        + "And I know I’ve reached the point of no return.",
+	        "Why did the database administrator leave his wife?\r\n"
+	        + "Because she had one-to-many relationships.\r\n",
+	        "Why do Linux users always code in the dark?\r\n"
+	        + "Because they don’t have windows.",
+	        // Add more jokes here
+	    };
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String str1 = request.getParameter("num1");
+		String str2 = request.getParameter("num2");
+		String str3 = request.getParameter("bt1");
+		
+		int a = Integer.parseInt(str1);
+		int b = Integer.parseInt(str2);
+		int ans;
+		
+		if(str3.equals("1")) ans= a+b;
+		else if(str3.equals("2")) ans=a-b;
+		else if(str3.equals("3")) ans=a*b;
+		else ans=a/b;
+		
+		 int randomIndex = (int) (Math.random() * jokes.length);
+	     String randomJoke = jokes[randomIndex];
+	     
+		response.sendRedirect("result.jsp?ans="+ans+"&joke="+randomJoke);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
